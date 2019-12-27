3393881a29ea615f89a912ea25af7fa961fea88d
NaoArimura <naoyosetani@gmail.com>
2019-12-27 19:27:28 +0900

    wrote Hello World in app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const http = require('http');
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+    console.log("Listening on port 3000")
+});
