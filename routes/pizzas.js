2ad9f2938b6f2004b9ced35892512bbe6b615233
NaoArimura <naoyosetani@gmail.com>
2019-12-29 16:33:16 +0900

    added pg-promise

---
+++ routes/pizzas.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const pgp = require("pg-promise")(/* options */);
+// const pgp = require("../app");
+const db = pgp("postgres://nao@host:5432/pizza_delivery");
+
+db.one("SELECT $1 AS value", 123)
+  .then(function(data) {
+    console.log("DATA:", data.value);
+  })
+  .catch(function(error) {
+    console.log("ERROR:", error);
+  });
+
+const pizzaData = router.get("/pizzas", (req, res, next) => {
+  res.status(200);
+  res.json(pizzaData);
+});
+
+module.exports = router;
